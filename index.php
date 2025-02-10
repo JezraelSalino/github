fa009b099d611ac623235ab66ffa59308811aeeb
JezraelSalino <jezraelsalino@gmail.com>
2025-02-10 22:32:04 +0800

    added new description from home page

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 <div class="container mx-auto py-8">
   <h1 class="text-3xl font-bold mb-4">Hello, GitHub!</h1>
   <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">New button here</button>
-  <p class="text-gray-600">This is a sample page using Tailwind CSS.</p>
+  <p class="text-gray-600 mt-5">New description from home page</p>
 </div>
 <?php include 'footer.php'; ?>
