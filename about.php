eb743c59c55e176dff36423666922c8cd6c44ed3
jezraelsalinokplus <jazrael.salino@knowledgeplus.net.au>
2025-09-30 15:20:16 +0800

    commit 1

--- about.php
+++ about.php
@@ -1,6 +1,6 @@
 <?php include 'header.php'; ?>
     <div class="container mx-auto py-8">
       <h1 class="text-3xl font-bold mb-4">Hello, from About Page ASD!</h1>
-      <p class="text-gray-600">This is a sample page using Tailwind CSSSSS.</p>
+      <p class="text-gray-600">This is a sample page using Tailwind CSS.</p>
     </div>
 <?php include 'footer.php'; ?>
